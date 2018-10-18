2529b89ee5116bde4ed1ac9f28c6ca83c0a6c0f9
beeric1 <beeric1@users.noreply.github.com>
2018-10-18 11:50:43 +0200

    design tweaks and basic fxml

--- src/mine/Controller.java
+++ src/mine/Controller.java
@@ -1,4 +1,44 @@
 package mine;
 
+import javafx.fxml.*;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+
 public class Controller {
+
+    //Game
+    @FXML
+    private MenuItem newgame;
+
+    @FXML
+    private MenuItem exitgame;
+
+    //Difficulty
+    @FXML
+    private MenuItem low;
+
+    @FXML
+    private MenuItem medium;
+
+    @FXML
+    private MenuItem high;
+
+    @FXML
+    private MenuItem custom;
+
+    //Status bar
+    @FXML
+    private Text mines;
+
+    @FXML
+    private Text time;
+
+    //Play ground
+    @FXML
+    private GridPane field;
+
+
+
 }
