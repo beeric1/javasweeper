427340a804cde94ccddba2688f1b9f1f2127e1aa
beeric1 <beeric1@users.noreply.github.com>
2018-10-24 16:50:26 +0200

    bottom controller added

---
+++ src/mine/BottomController.java
@@ -0,0 +1,15 @@
+package mine;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class BottomController {
+
+    //Status bar
+    @FXML
+    private Text mines;
+
+    @FXML
+    private Text time;
+
+}
